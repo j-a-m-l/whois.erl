3c599f8cea9dc7f87967a26a4a72cb52c29a24db
noijd <ju.an.marluc@gmail.com>
2015-01-19 10:13:47 +0000

    Fix whois:lookup: now uses the whois_server module as expected

--- src/whois.erl
+++ src/whois.erl
@@ -4,29 +4,16 @@
 
 -export([lookup/1, lookup/2]).
 
--define(DEFAULT_OPS, [{port, 43}, {timeout, 12000}]).
-
 %% -spec lookup(binary()) -> {ok, Response}.
 lookup(Domain) ->
     lookup(Domain, []).
 lookup(Domain, Ops) when is_list(Domain), is_list(Ops) ->
     lookup(list_to_binary(Domain), Ops);
 lookup(Domain, Ops) when is_binary(Domain), is_list(Ops) ->
-    gen_server:start_link({local, whois_server}, whois_server),
-    Pid = start(merge_options(Ops)),
-    receive
-      {ok, TldRecords} ->
-        other:action(TldRecords);
-      {error, Reason} ->
-        Reason
-    end,
-    Pid ! {whois, Domain},
-    gen_server:cast(whois_server, stop).
-
-%% TODO
-merge_options(_Ops) ->
-    ?DEFAULT_OPS.
-
+    whois_server:start_link(Ops),
+    Response = whois_server:lookup(Domain),
+    whois_server:stop(),
+    Response.
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
