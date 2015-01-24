a8975237d4d3b3329a4ea5991f51ebffef9dea02
noijd <ju.an.marluc@gmail.com>
2015-01-24 09:06:18 +0000

    Finished the first approach to downloading TLDs tests data

--- src/whois_data.erl
+++ src/whois_data.erl
@@ -4,9 +4,11 @@
 %%
 -module(whois_data).
 
+-include_lib("../include/whois_records.hrl").
+
 -export([save_test_data/1]).
 
--define(TEST_DATA_PATH, <<"test">>).
+-define(TEST_DATA_PATH, <<"test/data">>).
 
 %% TODO copied from whois_server
 -define(TLD_RE, <<"^.+\\.(\\w+)$">>).
@@ -18,17 +20,30 @@ extract_using_re(Data, State, Type) ->
         {match, [Result]} ->
             Result;
         _ ->
-            error
+            {error, extract_using_re, Data}
     end.
 
-save_test_data(Domain) ->
+save_test_data(Domain) when is_list(Domain) ->
+    save_test_data(list_to_binary(Domain));
+save_test_data(Domain) when is_binary(Domain) ->
     {ok, TldRe} = re:compile(?TLD_RE),
     Tld = extract_tld(Domain, [{tld_re, TldRe}]),
     File = <<?TEST_DATA_PATH/binary, "/", Tld/binary, "/", Domain/binary>>,
-    Data = whois:lookup(Domain),
-    save(File, Data).
+    Result = whois:lookup(Domain),
+    io:format(":~p~n", [Result]),
+    %% TODO create ?TEST_DATA_PATH if necessary
+    save(File, Result#whois.raw).
 
 save(File, Data) ->
+    Dir = filename:dirname(File),
+    case file:make_dir(Dir) of
+        ok              -> to_file(File, Data);
+        {error, eexist} -> to_file(File, Data);
+        Error           -> {error, save, File, Error}
+    end.
+
+to_file(File, Data) ->
+    %% TODO Data is empty
     {ok, Fd} = file:open(File, [raw, write]),
     file:write(Fd, Data),
-    file:close(Fd). 
+    file:close(Fd).
